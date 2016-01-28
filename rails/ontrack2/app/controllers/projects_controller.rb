ae560007ea943dd1812f08661f4cff2ae3724231
lebichmar <m.lebich@gmx.de>
2016-01-28 12:27:20 +0100

    Neues Projekt, erklärt von David und Max

---
+++ rails/ontrack2/app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+before_action :set_project, only: [:edit, :show, :update]
+
+def index
+	@projects = Project.all
+end
+
+def new
+	@project = Project.new
+end
+
+def show
+end
+
+def edit
+end
+
+
+def create
+	if 	@project = Project.create(permitted_params)
+			redirect_to project_path(@project.id)
+	else
+			render "new"
+	end	
+end
+
+def update
+@project.update(permitted_params)
+redirect_to root_path
+end
+
+
+def destroy
+	@project = Project.find(params [:id])
+end
+
+private
+
+	def permitted_params
+		params.require(:project).permit(:title, :description, :start_date)
+	end
+
+	def set_project
+		@project = Project.find(params[:id])
+	end
+
+
+
+end
---
+++ rails/ontrack2/db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+#Dokumentation
+
+# rake db:seed (führt den untenstehende Project.create-Befehle aus)
+#
+# Project.create(title: "Entwickeln und Programmieren", description: "bla bla", start_date: Time.now)
+#
+# Project.create(title: "Linux", description: "bla bla", start_date: Time.now)
+#
+# Project.create(title: "Planen und Installieren", description: "bla bla", start_date: Time.now)
+#
+# rake db:setup löscht alle Tabellen und erstellt sie neu
+
